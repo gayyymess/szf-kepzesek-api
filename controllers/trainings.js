39b45a09c34e5b702c919eb3d657b5b7b0320534
gayyymess <gayyymess>
2023-11-21 09:26:05 +0100

    added get getbyid and post functions

--- controllers/trainings.js
+++ controllers/trainings.js
@@ -1,20 +1,36 @@
+const Training = require('../models/training.js')
+
 // @desc   Get all trainings
 // @route  GET /api/trainings
 // @access Public
-exports.getTrainings = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "Show all trainings", hello: req.hello });
+exports.getTrainings = async (req, res, next) => {
+    const data = await Training.find();
+    res.json(data);
+    //res.status(200).json({ success: true, msg: "Show all trainings", hello: req.hello });
 };
 // @desc   Get single training
 // @route  GET /api/trainings/:id
 // @access Public
-exports.getTraining = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get training ${req.params.id}` });
+exports.getTraining = async (req, res, next) => {
+    const data = await Training.find()
+    Object.keys(data).forEach(key => {
+        if (data[key]["_id"] == req.params.id)
+           res.json(data[key])
+      });
 };
 // @desc   Create new training
 // @route  POST /api/trainings
 // @access Private
-exports.createTraining = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "Create new training" });
+exports.createTraining = async (req, res, next) => {
+    console.log(req.body);
+
+          try {
+            const training = await Training.create(req.body);
+            res.status(201).json({ success: true, data: training });
+          } catch (error) {
+            res.status(400).json({ success: false });
+          }
+    //res.status(200).json({ success: true, msg: "Create new training" });
 };
 // @desc   Update training
 // @route  PUT /api/trainings/:id
--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const trainings = require('./routes/trainings')
 
 require('dotenv').config() // A .env fájlt olvassa
 const app = express()
+
+const mongoString = process.env.CONNECTION_STRING
+
+mongoose.set('strictQuery', true)
+mongoose.connect(mongoString)
+const database = mongoose.connection
+
+database.on('error', (error)=>{
+    console.log(error)
+})
+
+database.once('connected', ()=>{
+    console.log('DB connected')
+})
+
+app.use(express.json())
+
 const morgan = require('morgan')
 
 app.use(morgan('dev'))
