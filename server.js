abf9ec6f4e5f0c5ff51eb346748bbace9e6a79ab
Hidasi Hanna <Hidasi.Hanna@jedlik.helyi>
2023-11-21 07:50:04 +0100

    first

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const trainings = require('./routes/trainings')
+
+require('dotenv').config() // A .env fájlt olvassa
+const app = express()
+const morgan = require('morgan')
+
+app.use(morgan('dev'))
+
+app.use('/api/trainings', trainings)
+
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
